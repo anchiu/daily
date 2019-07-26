d2045424b4f914591c56ae7523427c7eb329d825
terry <acchn@live.com>
2019-07-26 17:00:57 +0800

    add1.5

---
+++ primer/1.5/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	std::cout << "Enter two numbers:" << std::endl;
+	int v1 = 0,v2 = 0;
+	std::cout << "The sum of "<< v1 << " and " << v2 <<" is " <<std::endl;
+	std::cout << v1 + v2 <<std::endl;
+	return 0;
+}
