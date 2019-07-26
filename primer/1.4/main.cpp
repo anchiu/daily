45253e2c3f177004a1ce0e573b2c19d6bd260888
terry <acchn@live.com>
2019-07-26 16:53:33 +0800

    add 1.4

---
+++ primer/1.4/main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+ 
+int main() {
+	int a,b;	
+	cout << "请输入两个数字，中间用一个空格隔开" << endl; 
+	cin >> a;
+	cin >> b;
+	cout << "The result is  "<< a*b << endl;
+	return 0;
+}
