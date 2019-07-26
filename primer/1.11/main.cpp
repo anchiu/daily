5d674b0f1e4e48827b510726a5674ef22646f3f3
terry <acchn@live.com>
2019-07-26 20:45:40 +0800

    add1.11

---
+++ primer/1.11/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+    int v1,v2;
+    int i = 0;
+	int j;
+	int val;
+
+    std::cout << j << std::endl;
+	std::cout << "在下面输入两个整数，将为你显示两个整数中间的所有的整数 ：" <<std::endl;
+	std::cin >> v1 >> v2 ;
+	j = abs (v1-v2);
+	if (v1 < v2)
+	{
+		while (i < j)
+		{
+			std::cout << "当前数字是: " << v1 << std::endl;
+			++i; 
+			++v1;
+		}
+	} 
+	else 
+	{
+		while(i < j)
+		{
+			std::cout << "当前数字是 " << v1 << std::endl; 
+			++i;
+			--v1;
+		}
+	}
+	return 0;
+}
