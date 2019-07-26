f0e1657d7afde816ca629496d4bb5832e89af409
terry <acchn@live.com>
2019-07-26 16:42:42 +0800

    add 1.3

---
+++ primer/1.3/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using  namespace std; 
+int main(int argc, char** argv) {
+	
+	cout << "Hello World" << endl;
+	cout << "还是要一行一行代码写啊，不能眼高手低" ; 
+	return 0;
+}
