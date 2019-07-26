bb04e6bd3bc00974c6f204486631bb994049b8c9
terry <acchn@live.com>
2019-07-26 17:07:55 +0800

    add 1.6

---
+++ primer/1.6/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	std::cout << "The sum of " << v1;
+	          <<" and " << v2;
+			  <<"is "<< v1 +v2 << << std::endl; 
+	return 0;
+}
+
+//该程序段不合法，第一句结尾就加上了分号“；”，后面的语句就是另外的语句了
+//后面两句无法在输出流中实现。 
+
