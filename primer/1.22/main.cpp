e3042997c3988ff2b042321d044c83834fef8ad8
terry <acchn@live.com>
2019-07-27 10:08:27 +0800

    1.20

---
+++ primer/1.22/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Sales_item.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	Sales_item item1,item2;
+	std::cin >> item1 >> item2 ;
+	std::cout << item1 + item2 << std::endl;
+	return 0;
+}
+
+//Ò»ÑùµÄ
+ 
