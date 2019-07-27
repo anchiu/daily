fae6b2ba6c887881f7301ec87c781174c8975674
terry <acchn@live.com>
2019-07-27 22:47:25 +0800

    第一次在stackoverflow找到答案

--- primer/3/3.5/3.6/main.cpp
+++ primer/3/3.5/3.6/main.cpp
@@ -11,20 +11,27 @@
 using std::string;
  
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-char c;
+//char c;
 int i;
+int j;
 
 int main(int argc, char** argv) {
 	string str; //需要判断的字符串是 str 
+	
 	while(std::cin >> str)
 	{
-		for (auto &c : str)
+		j = str.length();
+		for (i = 0; i < j ; ++i)
 		{
 			//std::cout << c << std::endl;
-			str[c] = "X";	
+			str[i] = 'X';
+			//std::cout << "1" << std::endl;	
 		}	
 	std::cout << "当前的字符串是： " << str << std::endl; 
 	}
 		
 	return 0;
 }
+
+//碰到了一个问题卡了好久，最后在网上搜到了答案
+//https://stackoverflow.com/questions/12026567/error-invalid-conversion-from-const-char-to-char-fpermissive 
