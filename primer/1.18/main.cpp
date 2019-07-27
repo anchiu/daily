5b6c4713135a169581f131d62b2b1d53527e1d58
terry <acchn@live.com>
2019-07-27 08:26:19 +0800

    add 1.18

--- primer/1.18/main.cpp
+++ primer/1.18/main.cpp
@@ -5,18 +5,23 @@
 int main(int argc, char** argv) {
 	int currVal = 0; 
 	int val = 0;
-	while (std::cin >> currenVal)
+	if (std::cin >> currVal) 
 	{
-		if ()
+		int cnt = 1;
+		while (std::cin >> val)
 		{
-		
+			if(val == currVal)
+			{
+				cnt++;
+			}
+			else
+			{
+				std::cout << currVal << " occurs " << cnt << " times. " << std::endl;
+				currVal = val;
+				cnt = 1;
+			}
 		}	
-	
-		else 
-		{
-			
-		}
-		std::cout << currenVal << " occurs " << cnt << "times" << std::endl;
+		std::cout << currVal << " occurs " << cnt << " times. " << std::endl;
 	} 
 	return 0;
 }
