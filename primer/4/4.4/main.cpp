5df6e8d5dad2345ae3f5d4318d442a567480be98
terry <acchn@live.com>
2019-08-04 17:37:22 +0800

    4.4

---
+++ primer/4/4.4/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	std::cout << 12 / 3 * 4 +5 *15+24%4/2 <<std::endl;
+	return 0;
+}
+
+//½á¹ûÊÇ91 
