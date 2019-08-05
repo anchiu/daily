72e6980b777adb020cddde71512e5426e04560aa
terry <acchn@live.com>
2019-08-05 09:14:33 +0800

    1

---
+++ primer/10/10.1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm> 
+#include <vector>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	int x = 524; 
+	vector<int> v1 = {521,522,523,524,525};
+	auto result = find(v1.begin(),v1.end(),x);	
+	cout
+	return 0;
+}
