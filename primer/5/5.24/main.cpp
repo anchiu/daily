36450eb316058e10356b18b9c8a16109342c6ea9
terry <acchn@live.com>
2019-08-05 13:31:19 +0800

    1

---
+++ primer/5/5.24/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	int v1,v2;
+	cin >> v1 >> v2;	
+	cout << v1 /v2 << endl;
+	return 0;
+}
