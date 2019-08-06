464fdaf80622a8cfa55dfbd5a014679e26a12c75
pf201811281558819 <acchn@live.com>
2019-08-06 14:42:02 +0800

    add9.19

--- primer/9/9.18/main.cpp
+++ primer/9/9.18/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <string>
-#include <deque>
+#include <list>
 
 using namespace std;
  
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	deque<string> d1;
+	list<string> d1;
 	string s1;
 	cin >> s1;
 	d1.push_back(s1);
