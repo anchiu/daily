79fb7ee3e645b9e42c7944625e14c30ec2c95f6f
pf201811281558819 <acchn@live.com>
2019-08-06 16:07:17 +0800

    add9.16

--- primer/9/9.15/main.cpp
+++ primer/9/9.15/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <list>
 
 using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-bool compare(vector<int>& v3,vector<int>& v4)
+bool compare(list<int>& v3, list<int>& v4)
 {
 	if(v3 == v4)
 		return true;
@@ -14,8 +15,8 @@ bool compare(vector<int>& v3,vector<int>& v4)
 
 int main(int argc, char** argv)
 {
-	vector<int> v1 = {1,2,3,4,5};
-	vector<int> v2 = {1,2,3,4,5};
+	list<int> v1 = {1,2,3,4,5};
+	list<int> v2 = {1,2,3,4,5};
 
 	bool i = compare(v1,v2);
 	cout << i << endl;
