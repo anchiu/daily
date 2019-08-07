18ef06306c8aaac83bdf5e7c409db5031ad63f9e
pf201811281558819 <acchn@live.com>
2019-08-08 00:07:37 +0800

    add9.26

--- primer-answer/11/11.3/main.cpp
+++ primer-answer/11/11.3/main.cpp
@@ -18,34 +18,37 @@
 
 
 
-#include<iostream>  
-#include<string>  
-#include<fstream>
-#include<vector> 
-#include<map>  
-using namespace std;
- 
-int main(int argc, char**argv)  
-{ 
-	//map的定义
-	map<string,size_t> word_count;
-	fstream in("1.txt");//定义一个输入流
-	string word;
- 
-	while (in>>word)
-	{
-		++word_count[word];
-	}
- 
-	//map同样支持迭代器操作
-	map<string ,size_t>::iterator mapi;
-	for (mapi = word_count.begin(); mapi != word_count.end(); ++mapi)//C++ 11支持:const auto &s : word_count
-	{
-		//两个成员分别代表关键字和对应值
-		cout<<mapi->first<<" ";
-		cout<<mapi->second<<" "<<endl;
-	}
- 
-	return 0;  
-}  
+//#include<iostream>  
+//#include<string>  
+//#include<fstream>
+//#include<vector> 
+//#include<map>  
+//using namespace std;
+// 
+//int main(int argc, char**argv)  
+//{ 
+//	map的定义
+//	map<string,size_t> word_count;
+//	fstream in("1.txt");//定义一个输入流
+//	string word;
+// 
+//	while (in>>word)
+//	{
+//		++word_count[word];
+//	}
+// 
+//	map同样支持迭代器操作
+//	map<string ,size_t>::iterator mapi;
+//	for (mapi = word_count.begin(); mapi != word_count.end(); ++mapi)//C++ 11支持:const auto &s : word_count
+//	{
+//		两个成员分别代表关键字和对应值
+//		cout<<mapi->first<<" ";
+//		cout<<mapi->second<<" "<<endl;
+//	}
+//	
+//	return 0;  
+//}  
+
+
+//这题复习下 IO之后再做 
 
