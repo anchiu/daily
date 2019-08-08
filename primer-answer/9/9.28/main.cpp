5aa1466fd629987417af736978b4c35552b67ac1
pf201811281558819 <acchn@live.com>
2019-08-08 22:48:38 +0800

    一部分9.288

---
+++ primer-answer/9/9.28/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <forward_list>
+
+using namespace std;
+ 
+int main(int argc, char** argv) {
+
+	forward_list<string> f1;
+	string s1, s2;
+	
+	while (cin >> s1)
+	{
+		f1.push_back(s1)	
+	}
+	
+	while()
+	
+	
+
+	return 0;
+}
