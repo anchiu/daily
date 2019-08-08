f5fbbfc48ca34783c2f7b0561e2bba93f832804f
pf201811281558819 <acchn@live.com>
2019-08-08 23:00:45 +0800

    测试9.28

--- primer-answer/9/9.28/main.cpp
+++ primer-answer/9/9.28/main.cpp
@@ -1,22 +1,43 @@
-#include <iostream>
-#include <string>
-#include <forward_list>
-
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<vector>
+#include<forward_list>
 using namespace std;
  
-int main(int argc, char** argv) {
-
-	forward_list<string> f1;
-	string s1, s2;
-	
-	while (cin >> s1)
+void func(forward_list<string> &a, string b, string c)
+{
+	forward_list<string>::iterator it1 = a.before_begin();
+	forward_list<string>::iterator it2 = a.begin();
+ 
+	bool flag = false; 
+	for (it2; it2 != a.end(); it1 = it2++)
 	{
-		f1.push_back(s1)	
+		if (*it2 == b)
+		{
+			a.insert_after(it2,c);
+			flag = true;
+		}
 	}
-	
-	while()
-	
-	
-
+	if (!flag)
+	{
+		a.insert_after(it1,c);
+	}
+}
+ 
+int main(int argc, char**argv)
+{
+	forward_list<string> list1(4,"sadi");
+	string a = "sad5";
+	string b = "sad";
+ 
+	func(list1,a,b);
+ 
+	forward_list<string>::iterator it1 = list1.begin();
+	for (it1; it1!=list1.end();++it1)
+	{
+		cout<<*it1<<" ";
+	}
 	return 0;
 }
